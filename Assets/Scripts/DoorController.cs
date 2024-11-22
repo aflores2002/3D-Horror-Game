d0858490c26b35b84e173445d760afc050d0ef6c
aflores2002 <a3floresjr@gmail.com>
2024-11-22 14:54:06 -0800

    Added scripts that allow player to open door

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -1,18 +1,75 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
+// Controls door behavior and interaction with player
 public class DoorController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Animator animator;
+    private bool isInRange = false;  // Tracks if player is within interaction range
+    private bool isDoorOpen = false;
+
+    [Header("Debug")]
+    [SerializeField]
+    private bool showDebugMessages = true;  // Toggle debug logging
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    // Handles player input when in range of door
     void Update()
     {
-        
+        if (isInRange && Input.GetKeyDown(KeyCode.E) && !isDoorOpen)
+        {
+            TryOpenDoor();
+        }
+    }
+
+    // Checks if player has required fragments before opening door
+    void TryOpenDoor()
+    {
+        if (KeyFragmentManager.Instance.HasAllFragments())
+        {
+            OpenDoor();
+            if (showDebugMessages)
+            {
+                Debug.Log("Door opened successfully!");
+            }
+        }
+        else if (showDebugMessages)
+        {
+            Debug.Log("Need all fragments to open door!");
+        }
+    }
+
+    // Triggers door opening animation
+    void OpenDoor()
+    {
+        isDoorOpen = true;
+        animator.Play("Door_open");
+    }
+
+    // Detects when player enters interaction range
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInRange = true;
+            if (showDebugMessages)
+            {
+                Debug.Log("Player in range of door");
+            }
+        }
+    }
+
+    // Detects when player leaves interaction range
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInRange = false;
+        }
     }
 }
---
+++ Assets/Scripts/KeyFragmentManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// Manages key fragment collection using singleton pattern
+public class KeyFragmentManager : MonoBehaviour
+{
+    public static KeyFragmentManager Instance { get; private set; }
+
+    [SerializeField]
+    private bool testMode = true;  // Bypasses fragment requirement when enabled
+
+    private int collectedFragments = 0;
+    public int requiredFragments = 4;
+
+    void Awake()
+    {
+        // Singleton setup - maintains single instance across scenes
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Increments fragment count and logs progress
+    public void CollectFragment()
+    {
+        collectedFragments++;
+        Debug.Log($"Fragments collected: {collectedFragments}/{requiredFragments}");
+    }
+
+    // Returns true if all fragments collected or in test mode
+    public bool HasAllFragments()
+    {
+        return testMode || collectedFragments >= requiredFragments;
+    }
+}
