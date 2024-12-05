599d1f24fc8bffcdaeb4738bc506cce018ebd74f
gacalvo <gcalvo234@gmail.com>
2024-12-04 23:51:30 -0800

    fixed key locations
    Keys were getting permanently altered with locations reset to 0 when collected, I added a staticKeySpawn method called in Start() that sets the keys to specific locations at the beginning of every game

--- Assets/Scripts/KeyFragmentManager.cs
+++ Assets/Scripts/KeyFragmentManager.cs
@@ -7,7 +7,9 @@ using UnityEngine.Events;
 // Manages key fragment collection using singleton pattern (Will fully implement later)
 public class KeyFragmentManager : MonoBehaviour
 {
-    
+    //for spawning the keys
+    public GameObject keyPrefab;
+
     public static KeyFragmentManager Instance { get; private set; }
 
     public UnityEvent<KeyFragmentManager> OnKeyCollection;
@@ -32,6 +34,30 @@ public class KeyFragmentManager : MonoBehaviour
         }
     }
 
+    //spawn key locations at start 
+    void staticKeySpawn(GameObject key){
+        //set positions
+        Vector3 pos1 = new Vector3(-33.3f, 2.517f, 31.1f);
+        Vector3 pos2 = new Vector3(1.22f, 2.517f, -1.82f);
+        Vector3 pos3 = new Vector3(21.5f, 1.639f, -21.9f);
+        Vector3 pos4 = new Vector3(-9.7f, 2.26f, -17.6f);
+
+        //instantiate keys at specific positions
+        GameObject key1 = Instantiate(key, pos1, Quaternion.identity);
+        key1.transform.parent = null;
+        GameObject key2 = Instantiate(key, pos2, Quaternion.identity);
+        key2.transform.parent = null;
+        GameObject key3 = Instantiate(key, pos3, Quaternion.identity);
+        key3.transform.parent = null;
+        GameObject key4 = Instantiate(key, pos4, Quaternion.identity);
+        key4.transform.parent = null;
+
+    }
+
+    void Start(){
+        //spawn keys
+        staticKeySpawn(keyPrefab);
+    }
 
     // Increments fragment count and logs progress
     public void CollectFragment()
@@ -61,4 +87,5 @@ public class KeyFragmentManager : MonoBehaviour
         return collectedFragments;
     }
 
+    
 }
